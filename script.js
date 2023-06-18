ac49a04c0a25b1b8f44fdfcf2ac7d4ef240ff0a5
Serhii Yefimov <sergii.iefimov@gmail.com>
2023-06-18 23:36:40 +0300

    Added ligth and dark theme toggle

---
+++ script.js
@@ -0,0 +1,9 @@
+const toggleTheme = () => {
+  const themeBtn = document.querySelector(".theme-btn");
+  themeBtn.addEventListener("click", () => {
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+  });
+};
+
+toggleTheme();
